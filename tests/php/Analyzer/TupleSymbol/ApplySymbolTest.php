a53434033da9ebc2d992245fe0562e570315ed33
Jesús Valera Reales <jesus1902@outlook.com>
2020-07-14 23:06:07 +0200

    Add ApplySymbolTest & DefStructSymbolTest

---
+++ tests/php/Analyzer/TupleSymbol/ApplySymbolTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhelTest\Analyzer\TupleSymbol;
+
+use Phel\Analyzer;
+use Phel\Analyzer\TupleSymbol\ApplySymbol;
+use Phel\Ast\CallNode;
+use Phel\Exceptions\PhelCodeException;
+use Phel\GlobalEnvironment;
+use Phel\Lang\Symbol;
+use Phel\Lang\Tuple;
+use Phel\NodeEnvironment;
+use PHPUnit\Framework\TestCase;
+
+final class ApplySymbolTest extends TestCase
+{
+    private Analyzer $analyzer;
+
+    public function setUp(): void
+    {
+        $this->analyzer = new Analyzer(new GlobalEnvironment());
+    }
+
+    public function testLessThan3Arguments(): void
+    {
+        $this->expectException(PhelCodeException::class);
+        $this->expectExceptionMessage("At least three arguments are required for 'apply");
+
+        $tuple = Tuple::create(
+            Symbol::create(Symbol::NAME_APPLY),
+            Symbol::create('\\')
+        );
+        (new ApplySymbol($this->analyzer))($tuple, NodeEnvironment::empty());
+    }
+
+    public function testApplyNode(): void
+    {
+        $tuple = Tuple::create(
+            Symbol::create(Symbol::NAME_APPLY),
+            '+',
+            Tuple::create('1', '2', '3')
+        );
+        $applyNode = (new ApplySymbol($this->analyzer))($tuple, NodeEnvironment::empty());
+
+        self::assertSame('+', ($applyNode->getFn())->getValue());
+        self::assertInstanceOf(CallNode::class, $applyNode->getArguments()[0]);
+    }
+}
