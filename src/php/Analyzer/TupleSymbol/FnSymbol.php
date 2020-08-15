e29ce71df74dced9c28a4bac05de8e0b5f306714
Chemaclass <chemaclass@outlook.es>
2020-08-15 12:42:01 +0200

    Refactor FnSymbol

--- src/php/Analyzer/TupleSymbol/FnSymbol.php
+++ src/php/Analyzer/TupleSymbol/FnSymbol.php
@@ -6,6 +6,7 @@ namespace Phel\Analyzer\TupleSymbol;
 
 use Phel\Analyzer\WithAnalyzer;
 use Phel\Ast\FnNode;
+use Phel\Ast\Node;
 use Phel\Exceptions\AnalyzerException;
 use Phel\Lang\Symbol;
 use Phel\Lang\Tuple;
@@ -16,54 +17,79 @@ final class FnSymbol implements TupleSymbolAnalyzer
 {
     use WithAnalyzer;
 
+    private array $params = [];
+    private array $lets = [];
+    private bool $isVariadic = false;
+    private bool $hasVariadicForm = false;
+    private string $state = 'start';
+
     public function analyze(Tuple $tuple, NodeEnvironment $env): FnNode
     {
-        $tupleCount = count($tuple);
-        if ($tupleCount < 2) {
+        $this->verifyArguments($tuple);
+        $this->buildParams($tuple);
+        $this->addDummyVariadicSymbol();
+        $this->checkAllVariablesStartWithALetterOrUnderscore($tuple);
+
+        $recurFrame = new RecurFrame($this->params);
+
+        return new FnNode(
+            $env,
+            $this->params,
+            $this->analyzeBody($tuple, $recurFrame, $env),
+            $this->buildUsesFromEnv($env),
+            $this->isVariadic,
+            $recurFrame->isActive(),
+            $tuple->getStartLocation()
+        );
+    }
+
+    private function verifyArguments(Tuple $tuple): void
+    {
+        if (count($tuple) < 2) {
             throw AnalyzerException::withLocation("'fn requires at least one argument", $tuple);
         }
 
         if (!($tuple[1] instanceof Tuple)) {
             throw AnalyzerException::withLocation("Second argument of 'fn must be a Tuple", $tuple);
         }
+    }
 
-        $params = [];
-        $lets = [];
-        $isVariadic = false;
-        $hasVariadicForm = false;
-        $state = 'start';
+    private function buildParams(Tuple $tuple): void
+    {
+        /** @var Tuple $xs */
         $xs = $tuple[1];
+
         foreach ($xs as $param) {
-            switch ($state) {
+            switch ($this->state) {
                 case 'start':
                     if ($param instanceof Symbol) {
                         if ($this->isSymWithName($param, '&')) {
-                            $isVariadic = true;
-                            $state = 'rest';
+                            $this->isVariadic = true;
+                            $this->state = 'rest';
                         } elseif ($param->getName() === '_') {
-                            $params[] = Symbol::gen()->copyLocationFrom($param);
+                            $this->params[] = Symbol::gen()->copyLocationFrom($param);
                         } else {
-                            $params[] = $param;
+                            $this->params[] = $param;
                         }
                     } else {
                         $tempSym = Symbol::gen()->copyLocationFrom($param);
-                        $params[] = $tempSym;
-                        $lets[] = $param;
-                        $lets[] = $tempSym;
+                        $this->params[] = $tempSym;
+                        $this->lets[] = $param;
+                        $this->lets[] = $tempSym;
                     }
                     break;
                 case 'rest':
-                    $state = 'done';
-                    $hasVariadicForm = true;
+                    $this->state = 'done';
+                    $this->hasVariadicForm = true;
                     if ($this->isSymWithName($param, '_')) {
-                        $params[] = Symbol::gen()->copyLocationFrom($param);
+                        $this->params[] = Symbol::gen()->copyLocationFrom($param);
                     } elseif ($param instanceof Symbol) {
-                        $params[] = $param;
+                        $this->params[] = $param;
                     } else {
                         $tempSym = Symbol::gen()->copyLocationFrom($tuple);
-                        $params[] = $tempSym;
-                        $lets[] = $param;
-                        $lets[] = $tempSym;
+                        $this->params[] = $tempSym;
+                        $this->lets[] = $param;
+                        $this->lets[] = $tempSym;
                     }
                     break;
                 case 'done':
@@ -73,13 +99,24 @@ final class FnSymbol implements TupleSymbolAnalyzer
                     );
             }
         }
+    }
 
-        // Add a dummy variadic symbol
-        if ($isVariadic && !$hasVariadicForm) {
-            $params[] = Symbol::gen();
+    /** @param mixed $x */
+    private function isSymWithName($x, string $name): bool
+    {
+        return $x instanceof Symbol && $x->getName() === $name;
+    }
+
+    private function addDummyVariadicSymbol(): void
+    {
+        if ($this->isVariadic && !$this->hasVariadicForm) {
+            $this->params[] = Symbol::gen();
         }
+    }
 
-        foreach ($params as $param) {
+    private function checkAllVariablesStartWithALetterOrUnderscore(Tuple $tuple): void
+    {
+        foreach ($this->params as $param) {
             if (!(preg_match("/^[a-zA-Z_\x80-\xff].*$/", $param->getName()))) {
                 throw AnalyzerException::withLocation(
                     "Variable names must start with a letter or underscore: {$param->getName()}",
@@ -87,45 +124,43 @@ final class FnSymbol implements TupleSymbolAnalyzer
                 );
             }
         }
+    }
 
-        $recurFrame = new RecurFrame($params);
-        $body = array_slice($tuple->toArray(), 2);
+    private function analyzeBody(Tuple $tuple, RecurFrame $recurFrame, NodeEnvironment $env): Node
+    {
+        $tupleBody = array_slice($tuple->toArray(), 2);
 
-        if (count($lets) > 0) {
-            $body = Tuple::create(
-                (Symbol::create(Symbol::NAME_LET))->copyLocationFrom($body),
-                (new Tuple($lets, true))->copyLocationFrom($body),
-                ...$body
-            )->copyLocationFrom($body);
-        } else {
-            $body = Tuple::create(
-                (Symbol::create(Symbol::NAME_DO))->copyLocationFrom($body),
-                ...$body
-            )->copyLocationFrom($body);
-        }
+        $body = empty($this->lets)
+            ? $this->createDoTupleWithBody($tupleBody)
+            : $this->createLetTupleWithBody($tupleBody);
 
         $bodyEnv = $env
-            ->withMergedLocals($params)
+            ->withMergedLocals($this->params)
             ->withContext(NodeEnvironment::CTX_RET)
             ->withAddedRecurFrame($recurFrame);
 
-        $body = $this->analyzer->analyze($body, $bodyEnv);
-        $uses = array_diff($env->getLocals(), $params);
-
-        return new FnNode(
-            $env,
-            $params,
-            $body,
-            $uses,
-            $isVariadic,
-            $recurFrame->isActive(),
-            $tuple->getStartLocation()
-        );
+        return $this->analyzer->analyze($body, $bodyEnv);
     }
 
-    /** @param mixed $x */
-    private function isSymWithName($x, string $name): bool
+    private function createDoTupleWithBody(array $body): Tuple
     {
-        return $x instanceof Symbol && $x->getName() === $name;
+        return Tuple::create(
+            (Symbol::create(Symbol::NAME_DO))->copyLocationFrom($body),
+            ...$body
+        )->copyLocationFrom($body);
+    }
+
+    private function createLetTupleWithBody(array $tupleBody): Tuple
+    {
+        return Tuple::create(
+            (Symbol::create(Symbol::NAME_LET))->copyLocationFrom($tupleBody),
+            (new Tuple($this->lets, true))->copyLocationFrom($tupleBody),
+            ...$tupleBody
+        )->copyLocationFrom($tupleBody);
+    }
+
+    private function buildUsesFromEnv(NodeEnvironment $env): array
+    {
+        return array_diff($env->getLocals(), $this->params);
     }
 }
---
+++ tests/php/Analyzer/TupleSymbol/FnSymbolTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhelTest\Analyzer\TupleSymbol\FnSymbol;
+
+use Phel\Analyzer;
+use Phel\Analyzer\TupleSymbol\FnSymbol;
+use Phel\Exceptions\PhelCodeException;
+use Phel\GlobalEnvironment;
+use Phel\Lang\Symbol;
+use Phel\Lang\Tuple;
+use Phel\NodeEnvironment;
+use PHPUnit\Framework\TestCase;
+
+final class FnSymbolTest extends TestCase
+{
+    private Analyzer $analyzer;
+
+    public function setUp(): void
+    {
+        $this->analyzer = new Analyzer(new GlobalEnvironment());
+    }
+
+    public function testRequiresAtLeastOneArg(): void
+    {
+        $this->expectException(PhelCodeException::class);
+        $this->expectExceptionMessage("'fn requires at least one argument");
+
+        $tuple = Tuple::create();
+
+        (new FnSymbol($this->analyzer))->analyze($tuple, NodeEnvironment::empty());
+    }
+
+    public function testSecondArgMustBeATuple(): void
+    {
+        $this->expectException(PhelCodeException::class);
+        $this->expectExceptionMessage("Second argument of 'fn must be a Tuple");
+
+        $tuple = Tuple::create(
+            Symbol::create('unknown'),
+            Symbol::create('unknown')
+        );
+
+        (new FnSymbol($this->analyzer))->analyze($tuple, NodeEnvironment::empty());
+    }
+
+    public function testIsVariadic(): void
+    {
+        $tuple = Tuple::create(
+            Symbol::create('unknown'),
+            Tuple::create(Symbol::create('unknown'))
+        );
+
+        $fnNode = (new FnSymbol($this->analyzer))->analyze($tuple, NodeEnvironment::empty());
+
+        self::assertFalse($fnNode->isVariadic());
+    }
+}
