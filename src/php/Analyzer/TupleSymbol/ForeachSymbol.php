55ef0e705e75ddba3b45d9f9d0de95b161ff79aa
Chemaclass <chemaclass@outlook.es>
2020-08-22 13:21:14 +0200

    Refactor ForeachSymbol

--- src/php/Analyzer/TupleSymbol/ForeachSymbol.php
+++ src/php/Analyzer/TupleSymbol/ForeachSymbol.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Phel\Analyzer\TupleSymbol;
 
+use Phel\Analyzer\TupleSymbol\ReadModel\ForeachSymbolTuple;
 use Phel\Analyzer\WithAnalyzer;
 use Phel\Ast\ForeachNode;
 use Phel\Exceptions\AnalyzerException;
@@ -22,78 +23,108 @@ final class ForeachSymbol implements TupleSymbolAnalyzer
             throw AnalyzerException::withLocation("At least two arguments are required for 'foreach", $tuple);
         }
 
-        if (!($tuple[1] instanceof Tuple)) {
+        $foreachTuple = $tuple[1];
+        if (!($foreachTuple instanceof Tuple)) {
             throw AnalyzerException::withLocation("First argument of 'foreach must be a tuple.", $tuple);
         }
 
-        $firstArgCount = count($tuple[1]);
+        $firstArgCount = count($foreachTuple);
         if ($firstArgCount !== 2 && $firstArgCount !== 3) {
             throw AnalyzerException::withLocation("Tuple of 'foreach must have exactly two or three elements.", $tuple);
         }
 
-        $lets = [];
-        if (count($tuple[1]) === 2) {
-            $keySymbol = null;
-
-            $valueSymbol = $tuple[1][0];
-            if (!($valueSymbol instanceof Symbol)) {
-                $tmpSym = Symbol::gen();
-                $lets[] = $valueSymbol;
-                $lets[] = $tmpSym;
-                $valueSymbol = $tmpSym;
-            }
-            $bodyEnv = $env->withMergedLocals([$valueSymbol]);
-            $listExpr = $this->analyzer->analyze(
-                $tuple[1][1],
-                $env->withContext(NodeEnvironment::CTX_EXPR)
-            );
-        } else {
-            $keySymbol = $tuple[1][0];
-            if (!($keySymbol instanceof Symbol)) {
-                $tmpSym = Symbol::gen();
-                $lets[] = $keySymbol;
-                $lets[] = $tmpSym;
-                $keySymbol = $tmpSym;
-            }
-
-            $valueSymbol = $tuple[1][1];
-            if (!($valueSymbol instanceof Symbol)) {
-                $tmpSym = Symbol::gen();
-                $lets[] = $valueSymbol;
-                $lets[] = $tmpSym;
-                $valueSymbol = $tmpSym;
-            }
-
-            $bodyEnv = $env->withMergedLocals([$valueSymbol, $keySymbol]);
-            $listExpr = $this->analyzer->analyze(
-                $tuple[1][2],
-                $env->withContext(NodeEnvironment::CTX_EXPR)
-            );
-        }
-
-        $bodys = [];
-        for ($i = 2; $i < $tupleCount; $i++) {
-            $bodys[] = $tuple[$i];
-        }
-
-        if (count($lets)) {
-            $body = Tuple::create(Symbol::create(Symbol::NAME_LET), new Tuple($lets, true), ...$bodys);
-        } else {
-            $body = Tuple::create(Symbol::create(Symbol::NAME_DO), ...$bodys);
-        }
+        $foreachSymbolTuple = $this->buildForeachSymbolTuple($foreachTuple, $env);
 
         $bodyExpr = $this->analyzer->analyze(
-            $body,
-            $bodyEnv->withContext(NodeEnvironment::CTX_STMT)
+            $this->buildTupleBody($foreachSymbolTuple->lets(), $tuple),
+            $foreachSymbolTuple->bodyEnv()->withContext(NodeEnvironment::CTX_STMT)
         );
 
         return new ForeachNode(
             $env,
             $bodyExpr,
-            $listExpr,
-            $valueSymbol,
-            $keySymbol,
+            $foreachSymbolTuple->listExpr(),
+            $foreachSymbolTuple->valueSymbol(),
+            $foreachSymbolTuple->keySymbol(),
             $tuple->getStartLocation()
+        );
+    }
+
+    private function buildForeachSymbolTuple(Tuple $foreachTuple, NodeEnvironment $env): ForeachSymbolTuple
+    {
+        if (count($foreachTuple) === 2) {
+            return $this->buildForeachTupleWhen2Args($foreachTuple, $env);
+        }
+
+        return $this->buildForeachTupleWhen3Args($foreachTuple, $env);
+    }
+
+    private function buildForeachTupleWhen2Args(Tuple $foreachTuple, NodeEnvironment $env): ForeachSymbolTuple
+    {
+        $lets = [];
+        $valueSymbol = $foreachTuple[0];
+
+        if (!($valueSymbol instanceof Symbol)) {
+            $tmpSym = Symbol::gen();
+            $lets[] = $valueSymbol;
+            $lets[] = $tmpSym;
+            $valueSymbol = $tmpSym;
+        }
+        $bodyEnv = $env->withMergedLocals([$valueSymbol]);
+        $listExpr = $this->analyzer->analyze(
+            $foreachTuple[1],
+            $env->withContext(NodeEnvironment::CTX_EXPR)
+        );
+
+        return new ForeachSymbolTuple($lets, $bodyEnv, $listExpr, $valueSymbol);
+    }
+
+    private function buildForeachTupleWhen3Args(Tuple $foreachTuple, NodeEnvironment $env): ForeachSymbolTuple
+    {
+        $lets = [];
+        [$keySymbol, $valueSymbol] = $foreachTuple;
+
+        if (!($keySymbol instanceof Symbol)) {
+            $tmpSym = Symbol::gen();
+            $lets[] = $keySymbol;
+            $lets[] = $tmpSym;
+            $keySymbol = $tmpSym;
+        }
+
+        if (!($valueSymbol instanceof Symbol)) {
+            $tmpSym = Symbol::gen();
+            $lets[] = $valueSymbol;
+            $lets[] = $tmpSym;
+            $valueSymbol = $tmpSym;
+        }
+
+        $bodyEnv = $env->withMergedLocals([$valueSymbol, $keySymbol]);
+        $listExpr = $this->analyzer->analyze(
+            $foreachTuple[2],
+            $env->withContext(NodeEnvironment::CTX_EXPR)
+        );
+
+        return new ForeachSymbolTuple($lets, $bodyEnv, $listExpr, $valueSymbol, $keySymbol);
+    }
+
+    private function buildTupleBody(array $lets, Tuple $tuple): Tuple
+    {
+        $bodys = [];
+        for ($i = 2, $iMax = count($tuple); $i < $iMax; $i++) {
+            $bodys[] = $tuple[$i];
+        }
+
+        if (!empty($lets)) {
+            return Tuple::create(
+                Symbol::create(Symbol::NAME_LET),
+                new Tuple($lets, true),
+                ...$bodys
+            );
+        }
+
+        return Tuple::create(
+            Symbol::create(Symbol::NAME_DO),
+            ...$bodys
         );
     }
 }
