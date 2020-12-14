f12fafeafaa624114fd7594527d0444a895e1f72
Jens Haase <je.haase@gmail.com>
2020-12-14 20:14:17 +0100

    Unit tests for the Struct class

--- src/php/Lang/Table.php
+++ src/php/Lang/Table.php
@@ -18,7 +18,7 @@ class Table extends AbstractType implements ArrayAccess, Countable, Iterator, IS
 
     public static function empty(): Table
     {
-        return new self();
+        return new static();
     }
 
     /**
@@ -33,7 +33,7 @@ class Table extends AbstractType implements ArrayAccess, Countable, Iterator, IS
             throw new Exception('A even number of elements must be provided');
         }
 
-        $result = new self();
+        $result = new static();
         for ($i = 0, $l = count($kvs); $i < $l; $i += 2) {
             $result[$kvs[$i]] = $kvs[$i+1];
         }
