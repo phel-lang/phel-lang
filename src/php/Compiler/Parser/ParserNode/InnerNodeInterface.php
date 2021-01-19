3730b7c7690d905e0c282974b01a1a6596ae779e
Chemaclass <chemaclass@outlook.es>
2021-01-19 16:26:24 +0100

    Add declare_strict_types phpcs rule

--- src/php/Compiler/Parser/ParserNode/InnerNodeInterface.php
+++ src/php/Compiler/Parser/ParserNode/InnerNodeInterface.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Phel\Compiler\Parser\ParserNode;
 
 interface InnerNodeInterface extends NodeInterface
--- src/php/Compiler/Parser/ParserNode/ListNode.php
+++ src/php/Compiler/Parser/ParserNode/ListNode.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Phel\Compiler\Parser\ParserNode;
 
 use Phel\Compiler\Lexer\Token;
