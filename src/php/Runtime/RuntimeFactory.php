17f7ede12eaaf858d380314b5d8ff7301882388a
Chemaclass <chemaclass@outlook.es>
2021-02-02 20:04:36 +0100

    Create exceptions in Runtime module

--- src/php/Runtime/RuntimeFactory.php
+++ src/php/Runtime/RuntimeFactory.php
@@ -11,27 +11,34 @@ use Phel\Compiler\CompilerFactoryInterface;
 use Phel\Exceptions\ExceptionPrinterInterface;
 use Phel\Exceptions\HtmlExceptionPrinter;
 use Phel\Exceptions\TextExceptionPrinter;
-use RuntimeException;
+use Phel\Runtime\Exceptions\RuntimeAlreadyInitializedException;
+use Phel\Runtime\Exceptions\RuntimeNotInitializedException;
 
 final class RuntimeFactory
 {
     private static ?RuntimeInterface $instance = null;
 
+    /**
+     * @throws RuntimeNotInitializedException
+     */
     public static function getInstance(): RuntimeInterface
     {
         if (null === self::$instance) {
-            throw new RuntimeException('Runtime must first be initialized. Call Runtime::initialize()');
+            throw new RuntimeNotInitializedException();
         }
 
         return self::$instance;
     }
 
+    /**
+     * @throws RuntimeAlreadyInitializedException
+     */
     public static function initialize(
         ?GlobalEnvironmentInterface $globalEnv = null,
         ?string $cacheDirectory = null
     ): RuntimeInterface {
         if (self::$instance !== null) {
-            throw new RuntimeException('Runtime is already initialized');
+            throw new RuntimeAlreadyInitializedException();
         }
 
         self::$instance = new Runtime(
