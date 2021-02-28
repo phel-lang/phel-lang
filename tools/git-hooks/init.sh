3bda3588686e3487b9357ca2eef66a76751f7ad1
Chemaclass <chemaclass@outlook.es>
2021-02-28 19:44:04 +0100

    Adding pre-commit git hook to trigger all tests

---
+++ tools/git-hooks/init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+function setup_git_hooks()
+{
+  echo "Initialising git hooks..."
+  ln -sf "$PWD/tools/scripts/git-hooks/pre-commit.sh" "$PWD/.git/hooks/pre-commit"
+  echo "Done"
+}
+
+setup_git_hooks
---
+++ tools/git-hooks/pre-commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+echo 'composer test-all'
+composer test-all
+
