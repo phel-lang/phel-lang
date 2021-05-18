19f87b91c20ca4d258eed4dd77605959144d3502
Chemaclass <chemaclass@outlook.es>
2021-05-18 17:38:42 +0200

    Refactor ExportCommand. Removed NoFunctionsFoundException

--- src/php/Command/Export/ExportCommand.php
+++ src/php/Command/Export/ExportCommand.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Phel\Command\Export;
 
-use Phel\Command\Export\Exceptions\NoFunctionsFoundException;
 use Phel\Command\Shared\CommandExceptionWriterInterface;
 use Phel\Compiler\Exceptions\CompilerException;
 use Phel\Interop\InteropFacadeInterface;
-use Phel\Interop\ReadModel\Wrapper;
 use Phel\Runtime\RuntimeFacadeInterface;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
@@ -53,10 +51,6 @@ final class ExportCommand extends Command
             $this->createGeneratedWrappers($output);
 
             return self::SUCCESS;
-        } catch (NoFunctionsFoundException $e) {
-            $output->writeln($e->getMessage());
-
-            return self::SUCCESS;
         } catch (CompilerException $e) {
             $this->exceptionWriter->writeLocatedException($output, $e->getNestedException(), $e->getCodeSnippet());
         } catch (Throwable $e) {
@@ -69,35 +63,20 @@ final class ExportCommand extends Command
     /**
      * @throws CompilerException
      * @throws RuntimeException
-     * @throws NoFunctionsFoundException
      */
     private function createGeneratedWrappers(OutputInterface $output): void
     {
         $this->interopFacade->removeDestinationDir();
         $output->writeln('Exported namespaces:');
+        $wrappers = $this->interopFacade->generateWrappers();
 
-        foreach ($this->generateWrappers() as $i => $wrapper) {
+        if (empty($wrappers)) {
+            $output->writeln('No functions were found to be exported');
+        }
+
+        foreach ($wrappers as $i => $wrapper) {
             $this->interopFacade->createFileFromWrapper($wrapper);
             $output->writeln(sprintf('  %d) %s', $i + 1, $wrapper->relativeFilenamePath()));
         }
-    }
-
-    /**
-     * @throws RuntimeException
-     * @throws CompilerException
-     *
-     * @return list<Wrapper>
-     */
-    private function generateWrappers(): array
-    {
-        $wrappers = [];
-        foreach ($this->interopFacade->getFunctionsToExport() as $ns => $functionsToExport) {
-            $wrappers[] = $this->interopFacade->generateCompiledPhp($ns, $functionsToExport);
-        }
-        if (empty($wrappers)) {
-            throw new NoFunctionsFoundException();
-        }
-
-        return $wrappers;
     }
 }
