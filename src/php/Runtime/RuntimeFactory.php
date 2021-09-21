8e24f8039086ed8acad31f44332821abe720228c
JesusValera <jesus1902@outlook.com>
2021-09-21 20:49:13 +0200

    Fix Runtime exception

--- src/php/Runtime/RuntimeFactory.php
+++ src/php/Runtime/RuntimeFactory.php
@@ -5,30 +5,15 @@ declare(strict_types=1);
 namespace Phel\Runtime;
 
 use Gacela\Framework\AbstractFactory;
-use Phel\Runtime\Exceptions\PhelRuntimeException;
 
-/**
- * @method RuntimeConfig getConfig()
- */
 final class RuntimeFactory extends AbstractFactory
 {
-    /**
-     * @throws PhelRuntimeException
-     */
     public function getRuntime(): RuntimeInterface
     {
         if (RuntimeSingleton::isInitialized()) {
             return RuntimeSingleton::getInstance();
         }
 
-        $runtimePath = $this->getConfig()->getApplicationRootDir()
-            . DIRECTORY_SEPARATOR . 'vendor'
-            . DIRECTORY_SEPARATOR . 'PhelRuntime.php';
-
-        if (!file_exists($runtimePath)) {
-            throw PhelRuntimeException::couldNotBeLoadedFrom($runtimePath);
-        }
-
-        return require $runtimePath;
+        return RuntimeSingleton::initialize();
     }
 }
