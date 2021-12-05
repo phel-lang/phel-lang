d08f6322f4d1a88dcaa482e49adc1c3b351a2e7a
Chemaclass <chemaclass@outlook.es>
2021-12-05 14:17:22 +0100

    Refactor TryEmitter

--- src/php/Compiler/Emitter/OutputEmitter/NodeEmitter/TryEmitter.php
+++ src/php/Compiler/Emitter/OutputEmitter/NodeEmitter/TryEmitter.php
@@ -17,41 +17,53 @@ final class TryEmitter implements NodeEmitterInterface
     {
         assert($node instanceof TryNode);
 
-        if ($node->getFinally() || count($node->getCatches()) > 0) {
-            if ($node->getEnv()->getContext() === NodeEnvironmentInterface::CONTEXT_EXPRESSION) {
-                $this->outputEmitter->emitFnWrapPrefix($node->getEnv(), $node->getStartSourceLocation());
-            }
-
-            $this->outputEmitter->emitLine('try {', $node->getStartSourceLocation());
-            $this->outputEmitter->increaseIndentLevel();
+        if (!$node->getFinally() && count($node->getCatches()) === 0) {
             $this->outputEmitter->emitNode($node->getBody());
-            $this->outputEmitter->decreaseIndentLevel();
-            $this->outputEmitter->emitLine();
-            $this->outputEmitter->emitStr('}', $node->getStartSourceLocation());
+            return;
+        }
 
-            foreach ($node->getCatches() as $catchNode) {
-                $this->outputEmitter->emitNode($catchNode);
-            }
+        if ($node->getEnv()->getContext() === NodeEnvironmentInterface::CONTEXT_EXPRESSION) {
+            $this->outputEmitter->emitFnWrapPrefix($node->getEnv(), $node->getStartSourceLocation());
+        }
 
-            if ($node->getFinally()) {
-                $this->emitFinally($node->getFinally());
-            }
+        $this->emitTry($node);
+        $this->emitCatch($node);
+        $this->emitFinally($node);
 
-            if ($node->getEnv()->getContext() === NodeEnvironmentInterface::CONTEXT_EXPRESSION) {
-                $this->outputEmitter->emitFnWrapSuffix($node->getStartSourceLocation());
-            }
-        } else {
-            $this->outputEmitter->emitNode($node->getBody());
+        if ($node->getEnv()->getContext() === NodeEnvironmentInterface::CONTEXT_EXPRESSION) {
+            $this->outputEmitter->emitFnWrapSuffix($node->getStartSourceLocation());
         }
     }
 
-    private function emitFinally(AbstractNode $node): void
+    private function emitTry(TryNode $node): void
     {
-        $this->outputEmitter->emitLine(' finally {', $node->getStartSourceLocation());
+        $this->outputEmitter->emitLine('try {', $node->getStartSourceLocation());
         $this->outputEmitter->increaseIndentLevel();
-        $this->outputEmitter->emitNode($node);
+        $this->outputEmitter->emitNode($node->getBody());
         $this->outputEmitter->decreaseIndentLevel();
         $this->outputEmitter->emitLine();
         $this->outputEmitter->emitStr('}', $node->getStartSourceLocation());
+    }
+
+    private function emitCatch(TryNode $node): void
+    {
+        foreach ($node->getCatches() as $catchNode) {
+            $this->outputEmitter->emitNode($catchNode);
+        }
+    }
+
+    private function emitFinally(TryNode $node): void
+    {
+        $finally = $node->getFinally();
+        if (!$finally) {
+            return;
+        }
+
+        $this->outputEmitter->emitLine(' finally {', $finally->getStartSourceLocation());
+        $this->outputEmitter->increaseIndentLevel();
+        $this->outputEmitter->emitNode($finally);
+        $this->outputEmitter->decreaseIndentLevel();
+        $this->outputEmitter->emitLine();
+        $this->outputEmitter->emitStr('}', $finally->getStartSourceLocation());
     }
 }
