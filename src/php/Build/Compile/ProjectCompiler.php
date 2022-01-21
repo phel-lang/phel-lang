7bfe7ebd7539edeaa7d4355cd21be8e714e6df0b
Jens Haase <je.haase@gmail.com>
2022-01-21 22:27:26 +0100

    Suppress psalm error

--- src/php/Build/Compile/ProjectCompiler.php
+++ src/php/Build/Compile/ProjectCompiler.php
@@ -46,6 +46,7 @@ final class ProjectCompiler
                 && file_exists($targetFile)
                 && filemtime($targetFile) === filemtime($info->getFile())
             ) {
+                /** @psalm-suppress UnresolvableInclude */
                 require_once $targetFile;
                 continue;
             }
