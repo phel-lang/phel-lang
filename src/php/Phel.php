487aa290e1fa52c6c8334dfe01e554de6d942880
Chemaclass <chemaclass@outlook.es>
2022-05-21 10:00:18 +0200

    Unify phel config setup in a function Phel::configFn()

--- src/php/Phel.php
+++ src/php/Phel.php
@@ -10,18 +10,28 @@ use Phel\Run\RunFacade;
 
 final class Phel
 {
+    public const PHEL_CONFIG_FILE_NAME = 'phel-config.php';
+
+    private const PHEL_CONFIG_LOCAL_FILE_NAME = 'phel-config-local.php';
+
     /**
-     * This is a helper function to unify the run execution for a custom phel project.
+     * This function helps to unify the running execution for a custom phel project.
      */
     public static function run(string $projectRootDir, string $namespace): void
     {
-        $configFn = static function (GacelaConfig $config): void {
-            $config->addAppConfig('phel-config.php', 'phel-config-local.php');
-        };
-
-        Gacela::bootstrap($projectRootDir, $configFn);
+        Gacela::bootstrap($projectRootDir, self::configFn());
 
         $runFacade = new RunFacade();
         $runFacade->runNamespace($namespace);
+    }
+
+    /**
+     * @return callable(GacelaConfig):void
+     */
+    public static function configFn(): callable
+    {
+        return static function (GacelaConfig $config): void {
+            $config->addAppConfig(self::PHEL_CONFIG_FILE_NAME, self::PHEL_CONFIG_LOCAL_FILE_NAME);
+        };
     }
 }
