e433732476bcba974add4bb132573238355fa4fe
Chemaclass <chemaclass@outlook.es>
2023-01-28 19:13:52 +0100

    Simplify phel entrypoint

--- src/php/Console/ConsoleDependencyProvider.php
+++ src/php/Console/ConsoleDependencyProvider.php
@@ -7,6 +7,7 @@ namespace Phel\Console;
 use Gacela\Framework\AbstractDependencyProvider;
 use Gacela\Framework\Container\Container;
 use Phel\Build\Infrastructure\Command\BuildCommand;
+use Phel\Filesystem\FilesystemFacade;
 use Phel\Formatter\Infrastructure\Command\FormatCommand;
 use Phel\Interop\Infrastructure\Command\ExportCommand;
 use Phel\Run\Infrastructure\Command\ReplCommand;
@@ -16,8 +17,22 @@ use Phel\Run\Infrastructure\Command\TestCommand;
 final class ConsoleDependencyProvider extends AbstractDependencyProvider
 {
     public const COMMANDS = 'COMMANDS';
+    public const FACADE_FILESYSTEM = 'FACADE_FILESYSTEM';
 
     public function provideModuleDependencies(Container $container): void
+    {
+        $this->addFilesystemFacade($container);
+        $this->addCommands($container);
+    }
+
+    private function addFilesystemFacade(Container $container): void
+    {
+        $container->set(self::FACADE_FILESYSTEM, static function (Container $container) {
+            return $container->getLocator()->get(FilesystemFacade::class);
+        });
+    }
+
+    private function addCommands(Container $container): void
     {
         $container->set(self::COMMANDS, static fn () => [
             new ExportCommand(),
--- src/php/Console/Infrastructure/ConsoleBootstrap.php
+++ src/php/Console/Infrastructure/ConsoleBootstrap.php
@@ -8,6 +8,8 @@ use Gacela\Framework\DocBlockResolverAwareTrait;
 use Phel\Console\ConsoleFactory;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @method ConsoleFactory getFactory()
@@ -15,6 +17,15 @@ use Symfony\Component\Console\Command\Command;
 final class ConsoleBootstrap extends Application
 {
     use DocBlockResolverAwareTrait;
+
+    public function run(InputInterface $input = null, OutputInterface $output = null): int
+    {
+        $this->setAutoExit(false);
+        $exitCode = parent::run($input, $output);
+        $this->getFactory()->getFilesystemFacade()->clearAll();
+
+        return $exitCode;
+    }
 
     /**
      * @return array<string,Command>
