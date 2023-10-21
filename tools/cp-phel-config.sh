7af73e0c0afef3c8141c7f3924b1ec52c51ae6b7
Chemaclass <chemaclass@outlook.es>
2023-10-21 17:47:39 +0200

    cp phel config only if exists

--- tools/cp-phel-config.sh
+++ tools/cp-phel-config.sh
@@ -3,5 +3,8 @@
 current_dir="$(cd "$(dirname "$0")" && pwd)"
 vendor_dir="$current_dir/../vendor"
 phel_vendor_dir="$vendor_dir/phel-lang/phel-lang"
+phel_config_path="$phel_vendor_dir/phel-config.php"
 
-cp "$phel_vendor_dir/phel-config.php" $current_dir
+if [[ -e "$phel_config_path" ]]; then
+    cp "$phel_config_path" "$current_dir"
+fi
