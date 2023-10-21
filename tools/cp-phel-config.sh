5825038f74266b40819953727a921076199f6e37
Chemaclass <chemaclass@outlook.es>
2023-10-21 17:52:19 +0200

    wip debug

--- tools/cp-phel-config.sh
+++ tools/cp-phel-config.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "Copying phel-config..."
+
 current_dir="$(cd "$(dirname "$0")" && pwd)"
-vendor_dir="$current_dir/../vendor"
-phel_vendor_dir="$vendor_dir/phel-lang/phel-lang"
-phel_config_path="$phel_vendor_dir/phel-config.php"
+phel_config_path="$current_dir/../phel-config.php"
 
 if [[ -e "$phel_config_path" ]]; then
-    cp "$phel_config_path" "$current_dir"
+    cp "$phel_config_path" .
 fi
+
+echo "Done"
