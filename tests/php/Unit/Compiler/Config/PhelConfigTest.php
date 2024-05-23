911d62d7e6b0ae23edacf4fef57364122a163bf4
Chemaclass <chemaclass@outlook.es>
2024-05-23 16:25:03 +0200

    test: default_json_serialize

--- tests/php/Unit/Compiler/Config/PhelConfigTest.php
+++ tests/php/Unit/Compiler/Config/PhelConfigTest.php
@@ -11,20 +11,53 @@ use PHPUnit\Framework\TestCase;
 
 final class PhelConfigTest extends TestCase
 {
-    public function test_json_serialize(): void
+    public function test_default_json_serialize(): void
+    {
+        $config = new PhelConfig();
+
+        $expected = [
+            PhelConfig::SRC_DIRS => ['src'],
+            PhelConfig::TEST_DIRS => ['tests'],
+            PhelConfig::VENDOR_DIR => 'vendor',
+            PhelConfig::ERROR_LOG_FILE => 'data/error.log',
+            PhelConfig::BUILD_CONFIG => [
+                PhelBuildConfig::MAIN_PHEL_NAMESPACE => '',
+                PhelBuildConfig::DEST_DIR => 'out',
+                PhelBuildConfig::MAIN_PHP_FILENAME => 'index.php',
+                PhelBuildConfig::MAIN_PHP_PATH => 'out/index.php',
+            ],
+            PhelConfig::EXPORT_CONFIG => [
+                PhelExportConfig::TARGET_DIRECTORY => 'src/PhelGenerated',
+                PhelExportConfig::FROM_DIRECTORIES => ['src/phel'],
+                PhelExportConfig::NAMESPACE_PREFIX => 'PhelGenerated',
+            ],
+            PhelConfig::IGNORE_WHEN_BUILDING => ['src/phel/local.phel'],
+            PhelConfig::NO_CACHE_WHEN_BUILDING => [],
+            PhelConfig::KEEP_GENERATED_TEMP_FILES => false,
+            PhelConfig::FORMAT_DIRS => ['src', 'tests'],
+        ];
+
+        self::assertSame($expected, $config->jsonSerialize());
+    }
+
+    public function test_custom_json_serialize(): void
     {
         $config = (new PhelConfig())
             ->setSrcDirs(['some/directory'])
             ->setTestDirs(['another/directory'])
             ->setVendorDir('vendor')
             ->setErrorLogFile('error-log.file')
-            ->setBuildConfig((new PhelBuildConfig())
-                ->setMainPhpPath('out/custom-index.php')
-                ->setMainPhelNamespace('test-ns/boot'), )
-            ->setExportConfig((new PhelExportConfig())
-                ->setFromDirectories(['some/other/dir'])
-                ->setNamespacePrefix('Generated')
-                ->setTargetDirectory('src/Generated'))
+            ->setBuildConfig(
+                (new PhelBuildConfig())
+                    ->setMainPhpPath('out/custom-index.php')
+                    ->setMainPhelNamespace('test-ns/boot'),
+            )
+            ->setExportConfig(
+                (new PhelExportConfig())
+                    ->setFromDirectories(['some/other/dir'])
+                    ->setNamespacePrefix('Generated')
+                    ->setTargetDirectory('src/Generated'),
+            )
             ->setIgnoreWhenBuilding(['src/ignore.me'])
             ->setNoCacheWhenBuilding(['should-not-be-cached'])
             ->setKeepGeneratedTempFiles(true)
