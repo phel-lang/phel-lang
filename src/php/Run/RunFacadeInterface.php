69e272b2a53e63daa2a1ff0ba79e1fec9fd79c93
Chemaclass <chemaclass@outlook.es>
2024-11-16 15:45:32 +0100

    chore: add missing RunFacadeInterface methods

--- src/php/Run/RunFacadeInterface.php
+++ src/php/Run/RunFacadeInterface.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Phel\Run;
 
 use Phel\Build\Domain\Extractor\NamespaceInformation;
+use Phel\Compiler\Domain\Exceptions\CompilerException;
+use Phel\Compiler\Infrastructure\CompileOptions;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 interface RunFacadeInterface
 {
     public function runNamespace(string $namespace): void;
 
-    public function getNamespaceFromFile(string $fileOrPath): NamespaceInformation;
+    public function eval(string $phelCode, CompileOptions $compileOptions): mixed;
 
     /**
      * @return list<string>
@@ -18,12 +22,22 @@ interface RunFacadeInterface
     public function getAllPhelDirectories(): array;
 
     /**
-     * @param list<string> $directories
-     * @param list<string> $ns
+     * @param  list<string>  $directories
+     * @param  list<string>  $ns
      *
      * @return list<NamespaceInformation>
      */
     public function getDependenciesForNamespace(array $directories, array $ns): array;
 
     public function evalFile(NamespaceInformation $info): void;
+
+    public function registerExceptionHandler(): void;
+
+    public function getDependenciesFromPaths(array $paths): array;
+
+    public function getNamespaceFromFile(string $fileOrPath): NamespaceInformation;
+
+    public function writeLocatedException(OutputInterface $output, CompilerException $e): void;
+
+    public function writeStackTrace(OutputInterface $output, Throwable $e): void;
 }
