67b5ffa916b3d802d399b00a6f387935b208312d
smeghead <smeghead7@gmail.com>
2025-04-29 22:07:58 +0900

    fix: Fix `php/echo` does not work #729

---
+++ tests/php/Unit/Compiler/Emitter/OutputEmitter/NodeEmitter/CallEmitterTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhelTest\Unit\Compiler\Emitter\OutputEmitter\NodeEmitter;
+
+use Phel\Compiler\CompilerFactory;
+use Phel\Compiler\Domain\Analyzer\Ast\CallNode;
+use Phel\Compiler\Domain\Analyzer\Ast\LiteralNode;
+use Phel\Compiler\Domain\Analyzer\Ast\PhpVarNode;
+use Phel\Compiler\Domain\Analyzer\Environment\NodeEnvironment;
+use Phel\Compiler\Domain\Emitter\OutputEmitter\NodeEmitter\CallEmitter;
+use PHPUnit\Framework\TestCase;
+
+final class CallEmitterTest extends TestCase
+{
+    private CallEmitter $callEmitter;
+
+    protected function setUp(): void
+    {
+        $outputEmitter = (new CompilerFactory())
+            ->createOutputEmitter();
+
+        $this->callEmitter = new CallEmitter($outputEmitter);
+    }
+
+    public function test_php_var_node_print_language_constructs(): void
+    {
+        $node = new PhpVarNode(NodeEnvironment::empty(), 'print');
+        $args = [
+            new LiteralNode(NodeEnvironment::empty()->withExpressionContext(), 'abc'),
+        ];
+
+        $applyNode = new CallNode(NodeEnvironment::empty(), $node, $args);
+        $this->callEmitter->emit($applyNode);
+
+        $this->expectOutputString('print("abc");');
+    }
+
+    public function test_php_var_node_print_language_constructs_multi_auguments(): void
+    {
+        $node = new PhpVarNode(NodeEnvironment::empty(), 'print');
+        $args = [
+            new LiteralNode(NodeEnvironment::empty()->withExpressionContext(), 'abc'),
+            new LiteralNode(NodeEnvironment::empty()->withExpressionContext(), 'def'),
+        ];
+
+        $applyNode = new CallNode(NodeEnvironment::empty(), $node, $args);
+        $this->callEmitter->emit($applyNode);
+
+        $this->expectOutputString('print("abc", "def");');
+    }
+
+    public function test_php_var_node_echo_language_constructs(): void
+    {
+        $node = new PhpVarNode(NodeEnvironment::empty(), 'echo');
+        $args = [
+            new LiteralNode(NodeEnvironment::empty()->withExpressionContext(), 'abc'),
+        ];
+
+        $applyNode = new CallNode(NodeEnvironment::empty(), $node, $args);
+        $this->callEmitter->emit($applyNode);
+
+        $this->expectOutputString('print("abc");');
+    }
+
+}
