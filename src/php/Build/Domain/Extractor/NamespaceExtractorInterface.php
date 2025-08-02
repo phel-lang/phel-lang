ade77268ad53a93f7cbc181fd5f07efa6a3266f2
Chemaclass <chemaclass@outlook.es>
2025-08-02 12:38:39 +0200

    fix: linter

--- src/php/Build/Domain/Extractor/NamespaceExtractorInterface.php
+++ src/php/Build/Domain/Extractor/NamespaceExtractorInterface.php
@@ -11,7 +11,7 @@ interface NamespaceExtractorInterface
     /**
      * @param list<string> $directories
      *
-     * @return NamespaceInformation[]
+     * @return list<NamespaceInformation>
      */
     public function getNamespacesFromDirectories(array $directories): array;
 }
