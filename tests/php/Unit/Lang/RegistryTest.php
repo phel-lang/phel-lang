b7d7a8d88a20bc04daacf774d4c58466bfe5eb2b
Chemaclass <chemaclass@outlook.es>
2025-08-23 21:41:33 +0200

    rollback: RegistryTest to test Registry isolated

--- tests/php/Unit/Lang/RegistryTest.php
+++ tests/php/Unit/Lang/RegistryTest.php
@@ -4,33 +4,36 @@ declare(strict_types=1);
 
 namespace PhelTest\Unit\Lang;
 
-use Phel;
+use Phel\Lang\Registry;
 use PHPUnit\Framework\TestCase;
 
 final class RegistryTest extends TestCase
 {
+    private Registry $registry;
+
     public static function tearDownAfterClass(): void
     {
-        Phel::clear();
+        Registry::getInstance()->clear();
     }
 
     protected function setUp(): void
     {
-        Phel::clear();
+        $this->registry = Registry::getInstance();
+        $this->registry->clear();
     }
 
     public function test_null_when_non_existing_definition_by_value(): void
     {
-        $actual = Phel::getDefinition('ns', 'non-existing');
+        $actual = $this->registry->getDefinition('ns', 'non-existing');
 
         self::assertNull($actual);
     }
 
     public function test_value_definition(): void
     {
-        Phel::addDefinition('ns', 'array', [1, 2, 3]);
-        Phel::getDefinition('ns', 'array')[] = 4;
-        $actual = Phel::getDefinition('ns', 'array');
+        $this->registry->addDefinition('ns', 'array', [1, 2, 3]);
+        $this->registry->getDefinition('ns', 'array')[] = 4;
+        $actual = $this->registry->getDefinition('ns', 'array');
 
         self::assertSame([1, 2, 3], $actual);
     }
@@ -39,14 +42,14 @@ final class RegistryTest extends TestCase
     {
         $this->expectExceptionMessage('Only variables can be returned by reference');
 
-        Phel::getDefinitionReference('ns', 'non-existing');
+        $this->registry->getDefinitionReference('ns', 'non-existing');
     }
 
     public function test_reference_definition(): void
     {
-        Phel::addDefinition('ns', 'array', [1, 2, 3]);
-        Phel::getDefinitionReference('ns', 'array')[] = 4;
-        $actual = Phel::getDefinition('ns', 'array');
+        $this->registry->addDefinition('ns', 'array', [1, 2, 3]);
+        $this->registry->getDefinitionReference('ns', 'array')[] = 4;
+        $actual = $this->registry->getDefinition('ns', 'array');
 
         self::assertSame([1, 2, 3, 4], $actual);
     }
