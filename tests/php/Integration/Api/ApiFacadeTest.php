cbe1542936c66d76d3cd26e392d8bc31d7e32dde
Chemaclass <chemaclass@outlook.es>
2025-08-24 20:53:37 +0200

    feat: add median function to core.phel

--- tests/php/Integration/Api/ApiFacadeTest.php
+++ tests/php/Integration/Api/ApiFacadeTest.php
@@ -32,6 +32,6 @@ final class ApiFacadeTest extends TestCase
             ApiConfig::allNamespaces(),
         );
 
-        self::assertCount(328, $groupedFns);
+        self::assertCount(329, $groupedFns);
     }
 }
