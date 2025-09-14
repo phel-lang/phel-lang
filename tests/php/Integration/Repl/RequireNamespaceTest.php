9f4cb8d2c300b93a53c1e4edab12146babaafc6c
Chemaclass <chemaclass@outlook.es>
2025-09-14 13:46:35 +0200

    fix: repl require from repl.phel fails

---
+++ tests/php/Integration/Repl/RequireNamespaceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhelTest\Integration\Repl;
+
+use Gacela\Framework\Gacela;
+use Phel;
+use Phel\Build\BuildFacade;
+use Phel\Compiler\CompilerFacade;
+use Phel\Compiler\Infrastructure\CompileOptions;
+use Phel\Lang\Symbol;
+use PHPUnit\Framework\TestCase;
+
+final class RequireNamespaceTest extends TestCase
+{
+    public function test_require_loads_namespace(): void
+    {
+        Gacela::bootstrap(__DIR__);
+        Phel::addDefinition('phel\\repl', 'src-dirs', [__DIR__ . '/../../../../src']);
+
+        $build = new BuildFacade();
+        $build->evalFile(__DIR__ . '/../../../../src/phel/core.phel');
+        $build->evalFile(__DIR__ . '/../../../../src/phel/repl.phel');
+
+        $facade = new CompilerFacade();
+        $result = $facade->eval('(phel\\repl/require phel\\str)', new CompileOptions());
+
+        self::assertInstanceOf(Symbol::class, $result);
+        self::assertSame('phel\\str', $result->getFullName());
+    }
+}
