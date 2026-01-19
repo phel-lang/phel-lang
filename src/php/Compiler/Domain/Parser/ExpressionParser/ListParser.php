c8642aa13f160c71f221a8238dd1e25e350d5315
Chemaclass <chemaclass@outlook.es>
2026-01-19 15:53:50 +0100

    feat(errors): improve parser error messages with context
    Add helpful context to unterminated list/vector/map parser errors:
    - Include the line number where the unclosed structure started
    - Suggest the specific closing bracket that was expected (')', ']', or '}')
    
    Example: "Unterminated list starting at line 5. Did you forget a closing ')'?"

--- src/php/Compiler/Domain/Parser/ExpressionParser/ListParser.php
+++ src/php/Compiler/Domain/Parser/ExpressionParser/ListParser.php
@@ -5,12 +5,22 @@ declare(strict_types=1);
 namespace Phel\Compiler\Domain\Parser\ExpressionParser;
 
 use Phel\Compiler\Application\Parser;
+use Phel\Compiler\Domain\Exceptions\ErrorCode;
+use Phel\Compiler\Domain\Lexer\Token;
 use Phel\Compiler\Domain\Lexer\TokenStream;
 use Phel\Compiler\Domain\Parser\Exceptions\UnfinishedParserException;
 use Phel\Compiler\Domain\Parser\ParserNode\ListNode;
 
+use function sprintf;
+
 final readonly class ListParser
 {
+    private const array CLOSING_BRACKETS = [
+        Token::T_CLOSE_PARENTHESIS => ')',
+        Token::T_CLOSE_BRACKET => ']',
+        Token::T_CLOSE_BRACE => '}',
+    ];
+
     public function __construct(private Parser $parser)
     {
     }
@@ -21,7 +31,8 @@ final readonly class ListParser
     public function parse(TokenStream $tokenStream, int $endTokenType, int $tokenType): ListNode
     {
         $acc = [];
-        $startLocation = $tokenStream->current()->getStartLocation();
+        $startToken = $tokenStream->current();
+        $startLocation = $startToken->getStartLocation();
         $tokenStream->next();
 
         while ($tokenStream->valid()) {
@@ -37,6 +48,18 @@ final readonly class ListParser
             $acc[] = $this->parser->readExpression($tokenStream);
         }
 
-        throw UnfinishedParserException::forSnippet($tokenStream->getCodeSnippet(), $tokenStream->current(), 'Unterminated list');
+        $closingBracket = self::CLOSING_BRACKETS[$endTokenType] ?? ')';
+        $message = sprintf(
+            "Unterminated list starting at line %d. Did you forget a closing '%s'?",
+            $startLocation->getLine(),
+            $closingBracket,
+        );
+
+        throw UnfinishedParserException::forSnippet(
+            $tokenStream->getCodeSnippet(),
+            $tokenStream->current(),
+            $message,
+            ErrorCode::UNTERMINATED_LIST,
+        );
     }
 }
