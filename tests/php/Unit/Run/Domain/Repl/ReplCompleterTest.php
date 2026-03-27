be06178ba82faf7a377b8ba3fc4e194eb4f359ca
Chemaclass <chemaclass@outlook.es>
2026-03-27 21:23:53 +0100

    feat(repl): add type annotations to completion results
    Add completeWithTypes() method that returns CompletionResultTransfer
    objects with candidate name and type (function, macro, var, keyword,
    class, php-function) for nREPL client support (Cider, Calva).

--- tests/php/Unit/Run/Domain/Repl/ReplCompleterTest.php
+++ tests/php/Unit/Run/Domain/Repl/ReplCompleterTest.php
@@ -7,8 +7,10 @@ namespace PhelTest\Unit\Run\Domain\Repl;
 use Phel;
 use Phel\Api\Application\ReplCompleter;
 use Phel\Api\Domain\PhelFnLoaderInterface;
+use Phel\Api\Transfer\CompletionResultTransfer;
 use Phel\Compiler\Domain\Analyzer\Environment\GlobalEnvironment;
 use Phel\Lang\FnInterface;
+use Phel\Lang\Keyword;
 use Phel\Lang\Symbol;
 use PHPUnit\Framework\TestCase;
 
@@ -98,5 +100,169 @@ final class ReplCompleterTest extends TestCase
         $matches = $completer->complete('htm');
 
         self::assertContains('html', $matches);
+    }
+
+    public function test_empty_input_returns_nothing_with_types(): void
+    {
+        self::assertSame([], $this->completer->completeWithTypes(''));
+    }
+
+    public function test_function_has_function_type(): void
+    {
+        $fn = self::createStub(FnInterface::class);
+        Phel::addDefinition('phel\\core', 'myfn', $fn);
+
+        $results = $this->completer->completeWithTypes('my');
+
+        self::assertCount(1, $results);
+        self::assertInstanceOf(CompletionResultTransfer::class, $results[0]);
+        self::assertSame('myfn', $results[0]->candidate);
+        self::assertSame('function', $results[0]->type);
+    }
+
+    public function test_macro_has_macro_type(): void
+    {
+        $fn = self::createStub(FnInterface::class);
+        $meta = Phel::map(Keyword::create('macro'), true);
+        Phel::addDefinition('phel\\core', 'my-macro', $fn, $meta);
+
+        $results = $this->completer->completeWithTypes('my-mac');
+
+        self::assertCount(1, $results);
+        self::assertSame('my-macro', $results[0]->candidate);
+        self::assertSame('macro', $results[0]->type);
+    }
+
+    public function test_var_has_var_type(): void
+    {
+        Phel::addDefinition('phel\\core', 'my-var', 42);
+
+        $results = $this->completer->completeWithTypes('my-va');
+
+        self::assertCount(1, $results);
+        self::assertSame('my-var', $results[0]->candidate);
+        self::assertSame('var', $results[0]->type);
+    }
+
+    public function test_keyword_has_keyword_type(): void
+    {
+        Phel::addDefinition('phel\\core', 'my-kw', Keyword::create('test'));
+
+        $results = $this->completer->completeWithTypes('my-kw');
+
+        self::assertCount(1, $results);
+        self::assertSame('my-kw', $results[0]->candidate);
+        self::assertSame('keyword', $results[0]->type);
+    }
+
+    public function test_php_function_has_php_function_type(): void
+    {
+        $results = $this->completer->completeWithTypes('php/strl');
+
+        $strlen = null;
+        foreach ($results as $result) {
+            if ($result->candidate === 'php/strlen') {
+                $strlen = $result;
+                break;
+            }
+        }
+
+        self::assertNotNull($strlen);
+        self::assertSame('php-function', $strlen->type);
+    }
+
+    public function test_php_class_has_class_type(): void
+    {
+        $results = $this->completer->completeWithTypes('php/DateT');
+
+        $dateTime = null;
+        foreach ($results as $result) {
+            if ($result->candidate === 'php/DateTime') {
+                $dateTime = $result;
+                break;
+            }
+        }
+
+        self::assertNotNull($dateTime);
+        self::assertSame('class', $dateTime->type);
+    }
+
+    public function test_alias_completion_has_type(): void
+    {
+        $fn = self::createStub(FnInterface::class);
+        Phel::addDefinition('phel\\html', 'html', $fn);
+
+        $globalEnv = new GlobalEnvironment();
+        $globalEnv->setNs('user');
+        $globalEnv->addRequireAlias(
+            'user',
+            Symbol::create('h'),
+            Symbol::create('phel\\html'),
+        );
+
+        $phelFnLoader = self::createStub(PhelFnLoaderInterface::class);
+        $completer = new ReplCompleter($phelFnLoader, [], $globalEnv);
+
+        $results = $completer->completeWithTypes('h/ht');
+
+        self::assertCount(1, $results);
+        self::assertSame('h/html', $results[0]->candidate);
+        self::assertSame('function', $results[0]->type);
+    }
+
+    public function test_referred_symbol_has_type(): void
+    {
+        $fn = self::createStub(FnInterface::class);
+        Phel::addDefinition('phel\\html', 'html', $fn);
+
+        $globalEnv = new GlobalEnvironment();
+        $globalEnv->setNs('user');
+        $globalEnv->addRefer(
+            'user',
+            Symbol::create('html'),
+            Symbol::create('phel\\html'),
+        );
+
+        $phelFnLoader = self::createStub(PhelFnLoaderInterface::class);
+        $completer = new ReplCompleter($phelFnLoader, [], $globalEnv);
+
+        $results = $completer->completeWithTypes('htm');
+
+        self::assertCount(1, $results);
+        self::assertSame('html', $results[0]->candidate);
+        self::assertSame('function', $results[0]->type);
+    }
+
+    public function test_complete_still_returns_strings(): void
+    {
+        $fn = self::createStub(FnInterface::class);
+        Phel::addDefinition('phel\\core', 'myfn', $fn);
+
+        $results = $this->completer->complete('my');
+
+        self::assertSame(['myfn'], $results);
+        self::assertContainsOnly('string', $results);
+    }
+
+    public function test_completion_result_transfer_to_array(): void
+    {
+        $result = new CompletionResultTransfer('map', 'function');
+
+        self::assertSame([
+            'candidate' => 'map',
+            'type' => 'function',
+        ], $result->toArray());
+    }
+
+    public function test_qualified_function_in_non_core_namespace(): void
+    {
+        $fn = self::createStub(FnInterface::class);
+        Phel::addDefinition('phel\\str', 'join', $fn);
+
+        $results = $this->completer->completeWithTypes('phel\\str\\jo');
+
+        self::assertCount(1, $results);
+        self::assertSame('phel\\str\\join', $results[0]->candidate);
+        self::assertSame('function', $results[0]->type);
     }
 }
